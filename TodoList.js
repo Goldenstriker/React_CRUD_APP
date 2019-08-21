fd5bc55e2ea106ae92e1ba6689767ef80c772234
Goldenstriker <ndparab240@gmail.com>
2019-08-21 23:36:34 +0530

    2 files modified

--- TodoList.js
+++ TodoList.js
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 
 export default class TodoList extends Component {
   render() {
+    
     return (
       <React.Fragment>
       <ul>
-      {this.props.users.map(u=>{
-        <li key={u}>u</li>
-      })}
+      { this.props.loaded? this.props.users.map((u)=>{
+        <li key={u.id}>{u.id}</li>
+      }):null}
       </ul>
       <h1>TodlIst</h1>
       </React.Fragment>
--- index.js
+++ index.js
@@ -15,51 +15,60 @@ class App extends Component {
 
     this.state = {
       name: "React",
-      users:[]
+      users: [{id: 1, first_name: "Sebastian", last_name: "Eschweiler", email: "abc@mail.com"}],
+      loaded: true
     };
 
   }
-  componentDidMount() {
-        fetch(`https://my-json-server.typicode.com/Goldenstriker/schoolappdb/student`)
-            .then(res => res.json())
-            .then(result => this.setState({ users: result.users }))
-    }
-   todolist = [
+  /*componentDidMount() {
+    fetch(`https://my-json-server.typicode.com/Goldenstriker/schoolappdb/student`)
+      .then(res => res.json())
+      .then(result => { this.setState({ loaded: true }); console.log(this.state.loaded); console.log(result); this.setState({ users: result }); })
+  }*/
+  todolist = [
     { task: "ABC", id: 1 },
     { task: "DEF", id: 2 },
     { task: "GHI", id: 3 },
   ]
-   initialtodolist = {
+  initialtodolist = {
     task: "",
     id: 0
   };
   render() {
     return (
-      <Router>
-        <div className="container">
-          <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a class="navbar-brand" href="https://codingthesmartway.com" target="_blank">
-              <img src="https://cdn.jsdelivr.net/gh/Goldenstriker/React_CRUD_APP@master/logo.png" width="30" height="30" alt="CodingTheSmartWay.com" />
-            </a>
-            <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
-            <div className="collpase navbar-collapse">
-              <ul className="navbar-nav mr-auto">
-                <li className="navbar-item">
-                  <Link to="/" className="nav-link">Todos</Link>
-                </li>
-                <li className="navbar-item">
-                  <Link to="/create" className="nav-link">Create Todo</Link>
-                </li>
-              </ul>
-            </div>
-          </nav>
-          <br />
-          <TodoList users={this.state.users} />
-          <Route path="/" exact component={TodoList} />
-          <Route path="/edit/:id" component={EditTodo} />
-          <Route path="/create" component={CreateTodo} />
-        </div>
-      </Router>
+      <React.Fragment>
+        <Router>
+          <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+              <a className="navbar-brand" href="https://codingthesmartway.com" target="_blank">
+                <img src="https://cdn.jsdelivr.net/gh/Goldenstriker/React_CRUD_APP@master/logo.png" width="30" height="30" alt="CodingTheSmartWay.com" />
+              </a>
+              <Link to="/" className="navbar-brand">MERN-Stack Todo App</Link>
+              <div className="collpase navbar-collapse">
+                <ul className="navbar-nav mr-auto">
+                  <li className="navbar-item">
+                    <Link to="/" className="nav-link">Todos</Link>
+                  </li>
+                  <li className="navbar-item">
+                    <Link to="/create" className="nav-link">Create Todo</Link>
+                  </li>
+                </ul>
+              </div>
+            </nav>
+            <br />
+            <TodoList users={this.state.users} loaded={this.state.loaded} />
+            <Route path="/" exact component={TodoList} />
+            <Route path="/edit/:id" component={EditTodo} />
+            <Route path="/create" component={CreateTodo} />
+
+          </div>
+        </Router>
+        <ul>
+          {this.state.loaded ? this.state.users.map((u) => {
+            <li key={u.id}>{u.id}</li>
+          }) : null}
+        </ul>
+      </React.Fragment>
     );
   }
 }
