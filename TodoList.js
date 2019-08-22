01eb0bb0846ed5fabd67c11de82f7de521fa04fa
Goldenstriker <ndparab240@gmail.com>
2019-08-22 19:01:26 +0530

    2 files modified

--- TodoList.js
+++ TodoList.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 export default class TodoList extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      name: "React",
+      users: [],
+      loaded: false
+    };
+
+  }
+  componentDidMount() {
+    fetch(`https://my-json-server.typicode.com/Goldenstriker/schoolappdb/student`)
+      .then(res => res.json())
+      .then(result => { this.setState({ loaded: true }); console.log(this.state.loaded); console.log(result); this.setState({ users: result }); })
+  }
   render() {
     
     return (
       <React.Fragment>
       <ul>
-      { this.props.loaded? this.props.users.map((u)=>{
-        <li key={u.id}>{u.id}</li>
-      }):null}
+      { this.state.loaded? this.state.users.map((u)=>(
+        <li key={u.id}>{u.first_name}</li>
+      )):null}
       </ul>
       <h1>TodlIst</h1>
       </React.Fragment>
--- index.js
+++ index.js
@@ -14,17 +14,11 @@ class App extends Component {
     super();
 
     this.state = {
-      name: "React",
-      users: [{id: 1, first_name: "Sebastian", last_name: "Eschweiler", email: "abc@mail.com"}],
-      loaded: true
+      name: "React"
     };
 
   }
-  /*componentDidMount() {
-    fetch(`https://my-json-server.typicode.com/Goldenstriker/schoolappdb/student`)
-      .then(res => res.json())
-      .then(result => { this.setState({ loaded: true }); console.log(this.state.loaded); console.log(result); this.setState({ users: result }); })
-  }*/
+  
   todolist = [
     { task: "ABC", id: 1 },
     { task: "DEF", id: 2 },
@@ -56,18 +50,12 @@ class App extends Component {
               </div>
             </nav>
             <br />
-            <TodoList users={this.state.users} loaded={this.state.loaded} />
             <Route path="/" exact component={TodoList} />
             <Route path="/edit/:id" component={EditTodo} />
             <Route path="/create" component={CreateTodo} />
 
           </div>
         </Router>
-        <ul>
-          {this.state.loaded ? this.state.users.map((u) => {
-            <li key={u.id}>{u.id}</li>
-          }) : null}
-        </ul>
       </React.Fragment>
     );
   }
