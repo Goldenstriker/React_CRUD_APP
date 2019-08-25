f23ef2b596fb004f6dda7d2e4a4bd9e859b4c30f
Goldenstriker <ndparab240@gmail.com>
2019-08-25 21:18:16 +0530

    1 file modified, 1 added

---
+++ Loader.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from '.React';
+
+class Loader extends Component {
+  render() {
+    return (
+      < React.Fragment >
+        <div className="cssload-loader-inner">
+          <div className="cssload-cssload-loader-line-wrap-wrap">
+            <div className="cssload-loader-line-wrap"></div>
+          </div>
+          <div className="cssload-cssload-loader-line-wrap-wrap">
+            <div className="cssload-loader-line-wrap"></div>
+          </div>
+          <div className="cssload-cssload-loader-line-wrap-wrap">
+            <div className="cssload-loader-line-wrap"></div>
+          </div>
+          <div className="cssload-cssload-loader-line-wrap-wrap">
+            <div className="cssload-loader-line-wrap"></div>
+          </div>
+          <div className="cssload-cssload-loader-line-wrap-wrap">
+            <div className="cssload-loader-line-wrap"></div>
+          </div>
+        </div>
+      </React.Fragment>
+    );
+
+  };
+}
