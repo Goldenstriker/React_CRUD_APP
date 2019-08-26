db319c1f4fec7cdc58960cfc34baa2bae2d0d65e
Goldenstriker <ndparab240@gmail.com>
2019-08-26 20:00:09 +0530

    1 file modified

--- TodoList.js
+++ TodoList.js
@@ -15,13 +15,13 @@ export default class TodoList extends Component {
   componentDidMount() {
     fetch(`https://my-json-server.typicode.com/Goldenstriker/schoolappdb/student`)
       .then(res => res.json())
-      .then(result => { this.setState({ loaded: true }); console.log(this.state.loaded); console.log(result); this.setState({ users: result }); })
+      .then(result => { setTimeout(function(){this.setState({ loaded: true }); this.setState({ users: result });},5000); })
   }
   render() {
     
     return (
       <React.Fragment>
-      {!this.state.Loaded?<Loader/>:null}
+      {!this.state.loaded?<Loader/>:null}
       <ul>
       { this.state.loaded? this.state.users.map((u)=>(
         <li key={u.id}>{u.first_name}</li>
